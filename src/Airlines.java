44c0bdda07df654aa964a7f44da3c6f518572462
owermaiden <ower.maiden@cybertekschool.com>
2026-01-09 13:32:02 -0500

    &&

---
+++ src/Airlines.java
@@ -0,0 +1,21 @@
+public class Airlines {
+
+    public static void main(String[] args) {
+        String str = "Radar";
+
+    }
+
+    public static boolean isPalindrome(String str){
+        str = str.toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
